8c460397151a7dc4b3b22adb348f73d7db4d2d94
Trihunter2 <83244713+Trihunter2@users.noreply.github.com>
2026-01-18 10:16:01 +0000

    Daily Shop Names
    now on the right branch

--- mechgacha/shop.py
+++ mechgacha/shop.py
@@ -67,6 +67,32 @@ def format_shop_listing(item_string, item_index, cost):
     return f"""- `[{item_index+1}]` {item_string}
 -# **     **Cost: **{cost} scrap**"""
 
+def get_shop_info():
+    weekday_index = datetime.datetime.now(timezone_for_shopchange).weekday()
+    match weekday_index:
+        case 0: # Monday, body & back
+            today_shop_name = "Haydrian Mass Foundries"
+            today_shop_description = "The cycling of the Core has brought the Mech Gacha to a sprawling industrial park, conveniently aligned with Ratoon's home. The facility's open-plan design and endless iron supply makes it ideal for constructing bulkier components of mechs, such as Body and Back parts. An open hangar before the Mech Gacha holds some freshly-forged mech parts for trade:"
+        case 1: # Tuesday, arms & legs
+            today_shop_name = ""
+            today_shop_description = ""
+        case 2: # Wednesday, cockpit & power
+            today_shop_name = ""
+            today_shop_description = ""
+        case 3: # Thursday, weapons
+            today_shop_name = "Screwloose Botlabs"
+            today_shop_description = "The cycling of the Core has brought the Mech Gacha to the Combat Roblotics League campus, a compact arena intended for smaller RC robots to test their might. The Screwloose storefront holds a veritable arsenal of mech weaponry, optimized for winning through style, control, damage and aggression. Torus McGhee, eagerly manning the counter, revs with anticipation as you peruse your options:"
+        case 4: # Friday, kits
+            today_shop_name = ""
+            today_shop_description = ""
+        case 5: # Saturday, bodyplans
+            today_shop_name = "B.A.C.K. Market"
+            today_shop_description = "The cycling of the Core has brought the Mech Gacha to the B.A.C.K. Archives. Towering shelves loom overhead, holding endless mech blueprints and component Manuals. A handwritten note, beside a curated set of blueprints on a stand, simply reads: \"You may have a copy of these Body Plans, in exchange for an appropriate donation of scrap. M.B.\":"
+        case 6: # Sunday, special
+            today_shop_name = "[NULLIFIED]"
+            today_shop_description = "The cycling of the Core has brought the Mech Gacha to parts unknown. Suspended over blackest depths of the Pillars, a metal catwalk over the abyss holds an impromptu black market. Ethereal lighting illuminates the wares of the hooded shopkeeps. Trinkets and decals make up the majority of their collection, but a keen eye spots some worthwhile finds:"
+
+    return today_shop_name, today_shop_description
 
 def shop_cost(item):
 
@@ -97,6 +123,7 @@ def shop_cost(item):
 
 def view_shop(user_scrap="a competing standard (not yet accepted) of"):
     shop_choices = get_shop_items()
+    shop_name, shop_description = get_shop_info()
 
     now = datetime.datetime.now(timezone_for_shopchange)
     t = datetime.time(23, 59, 59, tzinfo=timezone_for_shopchange)
@@ -113,8 +140,8 @@ def view_shop(user_scrap="a competing standard (not yet accepted) of"):
 
     newline = '\n'
 
-    return f"""# **The Repair Shop**
-A "freshly renovated" sign beckons you into a wild array of half-disassembled bits and bobs with people at work all around you. Inside, several filing cabinets, car hoists, and horizontal painted doors have been combined to form display stands. Today, these items proudly sit on top of them:
+    return f"""# **{shop_name}**
+{shop_description}
 {newline.join([
     format_item_listing(item, i) 
     for i, item in enumerate(shop_choices)])}
